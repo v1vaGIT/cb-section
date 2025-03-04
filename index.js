d76db30796a891fcbf966e03f07eb70927968479
v1vaGIT <v1vatoxic@gmail.com>
2025-03-04 18:03:55 +1000

    commit

--- index.js
+++ index.js
@@ -9,20 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const firstBtnText = document.querySelector("#firstBtnText")
     const secondBtnText = document.querySelector("#secondBtnText")
 
-    function openText(button, text, btnText) {
+    const KarlashText = document.querySelector("#KarlashText")
+    const YvarovText = document.querySelector("#YvarovText")
+
+    function openText(button, text, btnText, type) {
         const opened = button.classList.contains("card-content__button-opened")
-        text.classList.toggle("card-content__opened")
         if (opened) {
             button.classList.remove("card-content__button-opened")
             button.classList.add("card-content__button-closed")
             btnText.textContent = "раскрыть"
+            if (type === "Karlash"){
+                    KarlashText.textContent = `“Уважаемые коллеги! Приветсвую участников совещания. Заявленная тема...`
+            } else {
+                    YvarovText.textContent = `“Уважаемые коллеги! Приветсвую участников совещания. Заявленная тема...`
+            }
         } else {
             button.classList.remove("card-content__button-closed")
             button.classList.add("card-content__button-opened")
             btnText.textContent = "скрыть"
+            if (type === "Karlash"){
+                    KarlashText.textContent = `“Уважаемые коллеги! Приветсвую участников совещания. Заявленная тема очень актуальна на сегодняшний день. Пусть наша встреча станет отправной точкой для новых идей и совместных проектов.“`
+            } else {
+                    YvarovText.textContent = `“Уважаемые коллеги! Приветсвую участников совещания. Заявленная тема очень актуальна на сегодняшний день.“`
+            }
         }
+        text.classList.toggle("card-content__opened")
     }
 
-    firstCard.addEventListener("click", ()=>openText(firstCard, firstText, firstBtnText))
-    secondCard.addEventListener("click", ()=>openText(secondCard, secondText, secondBtnText))
+    firstCard.addEventListener("click", ()=>openText(firstCard, firstText, firstBtnText, "Karlash"))
+    secondCard.addEventListener("click", ()=>openText(secondCard, secondText, secondBtnText, "Yvarov"))
 });
